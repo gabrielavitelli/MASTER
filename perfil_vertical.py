339e4735464162e46a846070ff38d51ec6176657
gabrielavitelli <gabrielavitelli@gmail.com>
2024-04-22 17:51:30 -0300

    lon e pressao ok theta ok rm quase ok

--- perfil_vertical.py
+++ perfil_vertical.py
@@ -10,21 +10,16 @@ from glob import glob
 import cartopy, cartopy.crs as ccrs
 import metpy.calc as mpcalc
 from metpy.units import units
+
 #######################################################################################################################################
 # le o arquivo 
 arquivo='/home/gabriela/treinamento/python/scripts/fig_dados/2024040100/gfs.2024040100.1p00.f024'
 
 # abre os arquivos
 ds = xr.open_dataset(arquivo, engine='cfgrib', filter_by_keys={'typeOfLevel': 'isobaricInhPa', 'stepType': 'instant'})#,'typeOfLevel': 'surface'})
-ds_rm = xr.open_dataset(arquivo, engine='cfgrib', filter_by_keys={'stepType': 'instant','typeOfLevel': 'surface'})#,'typeOfLevel': 'surface'})
 
-# recortando longitude
-ds_recortado =ds.sel(latitude=-25.0, longitude=slice(270, 335))
-ds_rm_recortado = ds_rm.sel(latitude=-25.0, longitude=slice(270, 335))
-
-# pegando valores para plotagem 
-longitude = ds['longitude'][25:91].values
-pressao = ds['isobaricInhPa'].values
+# recorta lat,lon,niveis atmosfericos
+ds_recortado =ds.sel(latitude=-25.0, longitude=slice(270, 335), isobaricInhPa=slice(1000, 200))
 
 #######################################################################################################################################
 # theta --> temperatura potencial # pt
@@ -33,9 +28,14 @@ p0 = 1000
 R = 287.05  
 cp = 1005  
 theta=[]
-pressoes_desejadas = np.array([1000, 900, 800, 700, 600, 500, 400, 300, 200])
-ds_interp = ds.interp(isobaricInhPa=pressoes_desejadas, method='linear')
-theta = ds_interp['t'] * (p0 / ds_interp['isobaricInhPa']) ** (R / cp)
+#pressoes_desejadas = np.array([1000, 900, 800, 700, 600, 500, 400, 300, 200])
+#ds_interp = ds.interp(isobaricInhPa=pressoes_desejadas, method='linear')
+#theta = ds_recortado['t'] * (p0 / ds_interp['isobaricInhPa']) ** (R / cp)
+
+theta = ds_recortado['t'] * (p0 / ds_recortado['isobaricInhPa']) ** (R / cp)
+
+print (theta)
+
 
 #######################################################################################################################################
 # velocidade vertical # 
@@ -47,10 +47,8 @@ v_1000 = ds['v'].sel(isobaricInhPa=1000).mean(dim=['longitude'])
 
 #######################################################################################################################################
 # razao de mistura # 
-rm=[]
-#rm = mixing_ratio(25 * units.hPa, 1000 * units.hPa).to('g/kg')
 
-T = ds_recortado['t']-273.15 #.values #em kelvin
+T = ds_recortado['t']-273.15 
 
 # pressao de vapor de saturacao
 pressao_vapor=  6.1078 * 10 ** ( (17.269*T) / (237.3+T) )
@@ -60,53 +58,37 @@ ur=ds_recortado['r']/100
 
 #pressa_vapor_real
 pressao_vr=pressao_vapor * ur
-
-
-
 pressao_vr_pascal = pressao_vr * units('hPa')
 
-print ('-------pressao vapor real ----------')
-print (pressao_vr_pascal)
-
 p_isobaric_pascal = ds_recortado['isobaricInhPa']*units('hPa')
-# Calculando a razão de mistura
+
+
+# razao de mistura
 w = ((pressao_vr_pascal * 0.622) / (p_isobaric_pascal - pressao_vr_pascal))
 
-print ('-------razao mistura ----------')
-print (w)
 
-quit()
+#print ('-------pressao vapor real ----------')
+#print (pressao_vr_pascal)
 
-ds_rm_recortado['pressao_vapor'] = (('isobaricInhPa', 'longitude'), pressao_vapor)
-ds_rm_recortado['pressao_vapor'] = ds_rm_recortado['pressao_vapor'].metpy.quantify()
-
-pressao_total= ds_rm_recortado['sp'].values
-
-
-razao_mistura = mpcalc.mixing_ratio(ds_rm_recortado['pressao_vapor']* units.pascal, pressao_total* units.pascal)
-
-print (razao_mistura)
+#print ('-------razao mistura ----------')
+#print (w)
 
 
 #######################################################################################################################################
 # plotagem #
 
-# ajusta a figura para ficar igual a do site do MASTER
-lon = np.where((longitude >= -90) & (longitude <= -25))[0]
-plon = np.where((pressao >= 200) & (pressao <= 1000))[0]  # Indices para pressão entre 200 e 1000 hPa
-
-# cria a grade
-Longitude, Pressao = np.meshgrid(longitude[lon], pressao[plon])  
 
 plt.figure(figsize=(10, 6))
 
-# plotar razao de mistura #
-plt.plot(razao_mistura, zorder=1)
+# cria a grade
+Longitude, Pressao = np.meshgrid(ds_recortado.longitude.values, ds_recortado.isobaricInhPa.values)  
 
-# plota theta
-#theta_recortado = theta.sel(latitude=-25.0, longitude=slice(270, 335))
-#contour = plt.contourf(theta_recortado['longitude'], theta_recortado['isobaricInhPa'], theta_recortado, levels=100, cmap='viridis')
-#plt.colorbar(contour, label='Temperatura Potencial (K)')
+contour = plt.contour(Longitude, Pressao, w, levels=15, colors='black') 
+plt.clabel(contour, inline=True, fontsize=10, fmt='%1.1f')  # Labels on contour lines
+
+
+contour = plt.contourf(theta.longitude, theta.isobaricInhPa, theta, cmap='viridis')
+plt.colorbar(contour, label='Temperatura Potencial (K)')
 
 plt.xlabel('longitude')
 plt.ylabel('Pressão (hPa)')
@@ -115,3 +97,4 @@ plt.gca().invert_yaxis()  # Inverte o eixo y para pressão
 plt.title('Perfil Vertical da razão de mistura')
 
 plt.show()
+plt.quit()
